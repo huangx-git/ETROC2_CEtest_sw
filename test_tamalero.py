1cdc35cf75a6753a42a98b2c8140650152c0b1a6
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2021-03-11 17:12:40 -0500

    some references

--- test_tamalero.py
+++ test_tamalero.py
@@ -19,5 +19,6 @@ if __name__ == '__main__':
 
     from tamalero.utils import get_temp
     
-    dac_29 = rb_0.SCA.ADC_read(29)/2**12
-    print ("\nTemperature on RB is: %.3f C"%get_temp(dac_29, 1.0, 10000, 25, 10000, 3900))
+    if rb_0.DAQ_LPGBT.rd_reg("LPGBT.RWF.CALIBRATION.VREFENABLE"):
+        dac_29 = rb_0.SCA.ADC_read(29)/2**12
+        print ("\nTemperature on RB is: %.3f C"%get_temp(dac_29, 1.0, 10000, 25, 10000, 3900))
