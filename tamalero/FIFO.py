926064bc4c8a76b32f38d31b3e9185bc63aa8d67
dspitzba <daniel.spitzbart@cern.ch>
2022-03-11 15:29:18 +0100

    fix word alignment issues in the data stream

--- tamalero/FIFO.py
+++ tamalero/FIFO.py
@@ -20,6 +20,10 @@ class FIFO:
         self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG0_MASK"%self.rb.rb, 0x00)
         self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG1"%self.rb.rb, 0x00)
         self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG1_MASK"%self.rb.rb, 0x00)
+        self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG2"%self.rb.rb, 0x00)
+        self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG2_MASK"%self.rb.rb, 0x00)
+        self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG3"%self.rb.rb, 0x00)
+        self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG3_MASK"%self.rb.rb, 0x00)
 
         with open(os.path.expandvars('$TAMALERO_BASE/configs/dataformat.yaml')) as f:
             self.dataformat = load(f, Loader=Loader)[ETROC]
@@ -31,11 +35,15 @@ class FIFO:
         self.rb.kcu.write_node("READOUT_BOARD_%s.LPGBT.DAQ.DOWNLINK.FAST_CMD_DATA"%self.rb.rb, self.fast_commands['L1A'])
         
 
-    def set_trigger(self, word0=0x0, word1=0x0, mask0=0x0, mask1=0x0):
+    def set_trigger(self, word0=0x0, word1=0x0, word2=0x0, word3=0x0, mask0=0x0, mask1=0x0, mask2=0x0, mask3=0x0):
         self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG0"%self.rb.rb, word0)
         self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG0_MASK"%self.rb.rb, mask0)
         self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG1"%self.rb.rb, word1)
         self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG1_MASK"%self.rb.rb, mask1)
+        self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG2"%self.rb.rb, word2)
+        self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG2_MASK"%self.rb.rb, mask2)
+        self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG3"%self.rb.rb, word3)
+        self.rb.kcu.write_node("READOUT_BOARD_%s.FIFO_TRIG3_MASK"%self.rb.rb, mask3)
         
 
     def reset(self):
@@ -48,26 +56,51 @@ class FIFO:
         if self.ETROC == 'ETROC2':
             self.rb.kcu.write_node("READOUT_BOARD_%s.LPGBT.DAQ.DOWNLINK.FAST_CMD_PULSE"%self.rb.rb, 0x01)  # FIXME confirm this
 
+    def make_word(self, bytes):
+        if len(bytes) == 5:
+            return bytes[0] << 32 | bytes[1] << 24 | bytes[2] << 16 | bytes[3] << 8 | bytes[4]
+        return 0
 
-    def dump(self, block=255):
+    def compare(self, byte, frame, mask):
+        return (byte & mask) == frame
+
+    def align_stream(self, stream):
+        frames = []
+        masks = []
+        for shift in [32, 24, 16, 8, 0]:
+            frames.append((self.dataformat['identifiers']['header']['frame'] & ((self.dataformat['identifiers']['header']['mask'] >> shift) & 0xFF) << shift) >> shift)
+            masks.append((self.dataformat['identifiers']['header']['mask'] >> shift) & 0xFF)
+
+        for i in range(250):
+            word = stream[i:i+5]
+            res = list(map(self.compare, word, frames, masks))
+            if sum(res) == 5:
+                return stream[i:]
+        return []
+
+    def dump(self, block=255, format=True):
         #self.rb.kcu.write_node("READOUT_BOARD_%s.LPGBT.DAQ.DOWNLINK.FAST_CMD_PULSE"%self.rb.rb, 0x01)  # FIXME this is not needed I think
         for i in range(10):
             if self.rb.kcu.read_node("READOUT_BOARD_%s.FIFO_EMPTY"%self.rb.rb).value() < 1: break
         res = self.rb.kcu.hw.getNode("DAQ_0.FIFO").readBlock(block)
         try:
             self.rb.kcu.hw.dispatch()
-            hex_dump = [ '{0:0{1}x}'.format(r,2) for r in res.value() ]
-            return hex_dump
+            return res.value()
         except:
             # NOTE: not entirely understood, but it seems this happens if FIFO is (suddenly?) empty
             return []
 
-
-    def giant_dump(self, block=3000, subblock=255):
-        res = []
+    def giant_dump(self, block=3000, subblock=255, format=True):
+        stream = []
         for i in range(block//subblock):
-            res += self.dump(block=subblock)
-        res += self.dump(block=block%subblock)
+            stream += self.dump(block=subblock, format=format)
+        stream += self.dump(block=block%subblock, format=format)
+        stream = self.align_stream(stream)
+        if format:
+            hex_dump = [ '{0:0{1}x}'.format(r,2) for r in stream ]
+            return hex_dump
+        else:
+            return [ self.make_word(c) for c in chunk(stream, n=5) if len(c)==5 ]
         return res
 
     def wipe(self, hex_dump, trigger_words=['35', '55'], integer=False):
@@ -80,8 +113,6 @@ class FIFO:
         for i in range(len(tmp_chunks)):
             if len(tmp_chunks[-1]) < self.dataformat['nbits']/8:
                 tmp_chunks.pop(-1)
-            elif tmp_chunks[-1][0:3] != ['95', '55', '55']:
-                tmp_chunks.pop(-1)
             else:
                 break
 
@@ -89,7 +120,6 @@ class FIFO:
             tmp_chunks = [ int(''.join(line),16) for line in tmp_chunks ]
 
         return tmp_chunks
-
 
     def dump_to_file(self, hex_dump, filename='dump.hex'):
         with open(filename, 'w') as f:
