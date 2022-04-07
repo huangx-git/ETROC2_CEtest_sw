e2a5bb0cb738a5216ce7e02f0b55fc7470d3abe8
Andrew Peck <andrew.peck@cern.ch>
2022-04-07 11:50:41 -0400

    print value of read after resetting MGT
    if the mgt is not working, the read will fail and return None, which
    will cause an error in hex() and quit the program before the MGT is ever
    reset
    
    With this change, the MGT will be reset first then checked with a small
    delay between the steps

--- test_tamalero.py
+++ test_tamalero.py
@@ -57,10 +57,11 @@ if __name__ == '__main__':
         print ("Power up init sequence for: DAQ")
         rb_0.DAQ_LPGBT.power_up_init()
         if (rb_0.DAQ_LPGBT.rd_adr(0x1c5) != 0xa5):
-            print(hex(rb_0.DAQ_LPGBT.rd_adr(0x1c5)))
             print ("No communication with DAQ LPGBT... trying to reset DAQ MGTs")
             rb_0.DAQ_LPGBT.reset_daq_mgts()
             rb_0.DAQ_LPGBT.power_up_init()
+            time.sleep(0.01)
+            print(hex(rb_0.DAQ_LPGBT.rd_adr(0x1c5)))
             #sys.exit(0)
         #rb_0.TRIG_LPGBT.power_up_init()
         rb_0.get_trigger()
