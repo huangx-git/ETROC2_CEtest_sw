36df625d945c2dbfc986e8d4be1c7458f8773464
So Young Jeon <so.young.jeon@cern.ch>
2022-06-18 11:31:27 -0400

    add ETROC class

---
+++ tamalero/ETROC.py
@@ -0,0 +1,13 @@
+"""
+For ETROC control
+"""
+
+class ETROC():
+
+    def __init__(self,
+            ):
+        #init
+
+    def connect_KCU(self, kcu):
+        # Need to connect to KCU105
+        self.kcu = kcu
