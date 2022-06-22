ac9fc1eaf1b061affbb66de3f16d97dc6a6bf245
So Young Jeon <so.young.jeon@cern.ch>
2022-06-22 13:22:38 -0400

    simple vth s curve emulator

--- ETROC_Emulator.py
+++ ETROC_Emulator.py
@@ -17,11 +17,12 @@ initialize(kcu_adr=args.kcu, force_no_trigger=args.force_no_trigger,
         read_fifo=args.read_fifo)
 """
 
-# software emulator...
+# software emulator
 
 # data storage
-data_stor = {0x0: 0,
-             0x1: 0,
+data_stor = {0x0: 0, # test register
+             0x1: 0, # vth
+             0x2: [] # accumulator nums
             }
 
 # emulating I2C connections
@@ -32,6 +33,8 @@ def I2C_write(reg, val):
 def I2C_read(reg):
     return data_stor[reg]
 
+# emulating runs
+
 
 # ===================================
 # ============== TEST ===============
@@ -39,8 +42,34 @@ def I2C_read(reg):
 
 from tamalero.ETROC import ETROC
 
+import numpy as np
+from matplotlib import pyplot as plt
+
 ETROCobj = ETROC(I2C_write, I2C_read)
 
+print("Test simple read/write...")
 ETROCobj.test_write(0x0, 1)
 testval = ETROCobj.test_read(0x0)
 print(testval)
+print("")
+
+N = 3200
+vth_min, vth_max = 190, 210
+vth_step = 1
+testpixel = 10
+print("Testing Vth S curve scan for pixel #%d..."%testpixel)
+
+vth_axis = np.arange(vth_min, vth_max, vth_step)
+acc_num_axis = np.zeros(vth_axis.size)
+
+for vth in range(vth_min, vth_max, vth_step):
+    ETROCobj.set_vth(vth)
+    ETROCobj.run(N);
+    i = int((vth-vth_min)/vth_step)
+    acc_num_axis[i] = ETROCobj.run_results()[testpixel]
+
+plt.title("S curve test") 
+plt.xlabel("Vth") 
+plt.ylabel("accumulation number") 
+plt.plot(vth_axis, acc_num_axis)
+plt.show()
--- tamalero/ETROC.py
+++ tamalero/ETROC.py
@@ -1,6 +1,7 @@
 """
 For ETROC control
 """
+import numpy as np
 
 class ETROC():
 
@@ -9,10 +10,33 @@ class ETROC():
         self.read  = read
 
     def test_write(self, reg, val):
-        print("Writing val %d to register %d"%(val, reg))
+        print("Writing to register %d"%reg)
         self.write(reg, val)
         return None
 
     def test_read(self, reg):
-        print("Reading val from register %d"%reg)
         return self.read(reg)
+
+    def set_vth(self, vth):
+        self.test_write(0x1, vth)
+        print("Vth set to %d."%vth)
+        return
+
+    def runpixel(self, N):
+        acc_num = 0
+        vth = self.test_read(0x1)
+        for i in range(N):
+            val = np.random.normal(198, 0.5)
+            if val > vth :
+                acc_num += 1
+        return acc_num
+
+    def run(self, N):
+        maxpixel = 16
+        alldata = [0 for x in range(maxpixel)]
+        for pixel in range(maxpixel):
+            alldata[pixel] = self.runpixel(N)
+        self.test_write(0x2, alldata)
+
+    def run_results(self):
+        return self.test_read(0x2)
