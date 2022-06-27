8146ae5ad074fbed8d1b734a4873d1817fe9bd5f
So Young Jeon <so.young.jeon@cern.ch>
2022-06-27 12:22:21 -0400

    add 2d hist of pixels

--- test_ETROC.py
+++ test_ETROC.py
@@ -32,9 +32,9 @@ vth_max  =  203
 vth_step =  .25 # step size
 N_steps  = int((vth_max-vth_min)/vth_step)+1 # number of steps
 N_pix    =   16 # total number of pixels
+N_pix_w  =    4 # N where pixels are in NxN layout
 
 # ===== HELPERS =====
-
 polyfit = np.polynomial.polynomial.polyfit
 
 def sigmoid(k,x,x0):
@@ -45,7 +45,7 @@ def sigmoid_log(ylist):
     fity = []
     for y in ylist:
         if abs(y) <= 0.001:
-            fity.append(10)
+            fity.append(np.inf)
         else:
             fity.append(np.log(1/y - 1))
     return np.array(fity)
@@ -57,20 +57,16 @@ def sigmoid_fit(x_axis,y_axis):
     x_axis_fit = []
     y_axis_fit = []
     for i in range(x_axis.size):
-        if abs(y_axis[i]) > 7:
-            print("delete")
-            print(y_axis[i])
-        else:
+        # only keep values within linearly shaped range
+        if abs(y_axis[i]) < 3:
             x_axis_fit.append(x_axis[i])
             y_axis_fit.append(y_axis[i])
-    print(x_axis_fit)
-    print(y_axis_fit)
     results = polyfit(x_axis_fit, y_axis_fit, 1)
     kx0, k = results[0], results[1]
     x0 = - kx0 / k
     return (k, x0)
 
-# do vth scan
+# ===== Vth scan ====
 vth_axis    = np.linspace(vth_min, vth_max, N_steps)
 run_results = np.empty([N_steps, N_pix])
 
@@ -78,37 +74,44 @@ for vth in vth_axis:
     ETROCobj.set_vth(vth)
     ETROCobj.run(N_l1a);
     i = int(round((vth-vth_min)/vth_step))
-    print(i)
     run_results[i] = ETROCobj.run_results()
-    print(run_results[i])
 
 # transpose so each 1d list is for a pixel
 # also normalize
 run_results = run_results.transpose()/N_l1a
 
 # fit to sigmoid
-widths = np.empty(N_pix)
-means  = np.empty(N_pix)
-guess = [2, 5] # starting guess for fit
+widths = np.empty([N_pix_w, N_pix_w])
+means  = np.empty([N_pix_w, N_pix_w])
+#widths = np.empty(N_pix)
+#means  = np.empty(N_pix)
 
 for pix in range(N_pix):
     print("for pixel #%d"%pix)
     fitresults = sigmoid_fit(vth_axis, run_results[pix])
     print(fitresults)
-    widths[pix] = fitresults[0]
-    means[pix] = fitresults[1]
+    r = pix%N_pix_w
+    c = int(np.floor(pix/N_pix_w))
+    print("row = %d, col = %d"%(r,c))
+    widths[r][c] = fitresults[0]
+    means[r][c]  = fitresults[1]
 
     #popt, pcov = curve_fit(sigmoid, fitx, run_results[pix],  method="trf", bounds=([1,1],[9,5]))
     #widths[pix] = popt[0] #k
     #means[pix] = popt[1] #x0
     #print("fit = "+str(popt))
 
-# example result and fit
-plt.title("S curve test") 
+# example fit result
+plt.title("S curve fit example (pixel #0)") 
 plt.xlabel("Vth") 
-plt.ylabel("accumulation number") 
+plt.ylabel("hit percentage") 
 plt.plot(vth_axis, run_results[0])
-fit_func = sigmoid(widths[0], vth_axis, means[0])
+fit_func = sigmoid(widths[0][0], vth_axis, means[0][0])
 plt.plot(vth_axis, fit_func)
-plt.legend(["data","fit","guess"])
+plt.legend(["data","fit"])
+
+# 2D histogram
+
+plt.title("Mean values of baseline voltage")
+plt.matshow(widths)
 plt.show()
