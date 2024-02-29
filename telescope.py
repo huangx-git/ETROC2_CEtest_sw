1b80a2ae283a477d1e623a6086dc722344f859fa
dspitzba <daniel.spitzbart@cern.ch>
2024-02-29 13:51:31 -0500

    WIP telescope code

--- telescope.py
+++ telescope.py
@@ -29,6 +29,9 @@ layers = [
 
 if __name__ == '__main__':
     shut_down = False
+    connect_modules = False
+    #kcu = '192.168.0.10'
+    kcu = '192.168.0.13'
 
     print(emojize(':atom_symbol:'), " Telescope code draft")
 
@@ -42,43 +45,46 @@ if __name__ == '__main__':
     print(emojize(":check_mark_button:"), " Ready")
 
     print("Getting the KCU")
-    kcu = get_kcu('192.168.0.10', control_hub=True, verbose=True)
+    kcu = get_kcu(kcu, control_hub=True, verbose=True)
 
     print("Setting up Readout Boards")
     rb_0 = ReadoutBoard(0, kcu=kcu, config='modulev0b', verbose=False)
-    rb_1 = ReadoutBoard(1, kcu=kcu, config='modulev0b', verbose=False)
-    #rb_2 = ReadoutBoard(2, kcu=kcu, config='modulev0b', verbose=False)  # links not getting ready yet
-    rb_2 = ReadoutBoard(2, kcu=kcu, config='modulev0b', verbose=False)
+    #rb_1 = ReadoutBoard(1, kcu=kcu, config='modulev0b', verbose=False)
+    #rb_2 = ReadoutBoard(2, kcu=kcu, config='modulev0b', verbose=False)
+    #rb_3 = ReadoutBoard(3, kcu=kcu, config='modulev0b', verbose=False)
+    #rb_4 = ReadoutBoard(4, kcu=kcu, config='modulev0b', verbose=False)
 
-    print("Connecting Modules")
-    rb_0.connect_modules()
-    rb_1.connect_modules()
-    rb_2.connect_modules()
+    if connect_modules:
 
-    print("Showing status")
-    rb_0.modules[0].show_status()
-    #rb_0.modules[2].show_status()
-    rb_1.modules[1].show_status()
+        print("Connecting Modules")
+        rb_0.connect_modules()
+        rb_1.connect_modules()
+        rb_2.connect_modules()
 
-    rb_0.modules[0].ETROCs[0].test_config(occupancy=127)
-    #rb_0.modules[2].ETROCs[0].test_config(occupancy=127)
-    rb_1.modules[1].ETROCs[0].test_config(occupancy=127)
+        print("Showing status")
+        rb_0.modules[0].show_status()
+        #rb_0.modules[2].show_status()
+        rb_1.modules[1].show_status()
 
-    fifo_0 = FIFO(rb_0)
-    fifo_1 = FIFO(rb_1)
-    df = DataFrame("ETROC2")
+        rb_0.modules[0].ETROCs[0].test_config(occupancy=127)
+        #rb_0.modules[2].ETROCs[0].test_config(occupancy=127)
+        rb_1.modules[1].ETROCs[0].test_config(occupancy=127)
 
-    # doesn't matter which FIFO to choose, the L1A is universial
-    print(emojize(':factory:'), " Producing data")
-    fifo_0.send_l1a(2)
+        fifo_0 = FIFO(rb_0)
+        fifo_1 = FIFO(rb_1)
+        df = DataFrame("ETROC2")
 
-    print(emojize(':closed_mailbox_with_raised_flag:'), " Data in FIFO 0:")
-    for x in fifo_0.pretty_read(df):
-        print(x)
+        # doesn't matter which FIFO to choose, the L1A is universial
+        print(emojize(':factory:'), " Producing data")
+        fifo_0.send_l1a(2)
 
-    print(emojize(':closed_mailbox_with_raised_flag:'), " Data in FIFO 1:")
-    for x in fifo_1.pretty_read(df):
-        print(x)
+        print(emojize(':closed_mailbox_with_raised_flag:'), " Data in FIFO 0:")
+        for x in fifo_0.pretty_read(df):
+            print(x)
+
+        print(emojize(':closed_mailbox_with_raised_flag:'), " Data in FIFO 1:")
+        for x in fifo_1.pretty_read(df):
+            print(x)
 
     if shut_down:
         psu1.power_down('ch1')
