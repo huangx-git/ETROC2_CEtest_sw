34c92a1646e747bc452d70625fce2473cda5d4ab
dspitzba <daniel.spitzbart@cern.ch>
2024-03-07 10:01:14 -0500

    more paths

--- autopilot.sh
+++ autopilot.sh
@@ -6,9 +6,9 @@ ipython3 fnal_laser_test.py -- --kcu 192.168.0.10 --hard_reset --offset $2
 echo -n "True" > running_ETROC_acquisition.txt
 (python3 daq.py --l1a_rate 0 --ext_l1a --kcu 192.168.0.10 --run $index --lock "$TAMALERO_BASE/../ScopeHandler/Lecroy/Acquisition/running_acquitision.txt") &
 (sleep 15
-python3 ~/ETROC2_Test_Stand/ScopeHandler/Lecroy/Acquisition/acquisition_wrapper.py $1)
+python3 $TAMALERO_BASE/../ScopeHandler/Lecroy/Acquisition/acquisition_wrapper.py $1)
 python3 data_dumper.py --input $index
 python3 root_dumper.py --input $index
 echo -n "False" > running_ETROC_acquisition.txt
 echo -n "True" > merging.txt
-echo -n "True" > ~/ETROC2_Test_Stand/ScopeHandler/Lecroy/Acquisition/merging.txt
+echo -n "True" > $TAMALERO_BASE/../ScopeHandler/Lecroy/Acquisition/merging.txt
