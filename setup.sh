2dc3794b2535c755aaa4bbd7f82e3203ef6a2c2a
dspitzba <daniel.spitzbart@cern.ch>
2024-03-07 10:04:10 -0500

    making result directories

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ echo "Set PYTHONPATH"
 echo $PYTHONPATH
 echo "Set LD_LIBRARY"
 echo $LD_LIBRARY_PATH
+
+# making some of the directories that are (currently) needed
+mkdir -p output
+mkdir -p ETROC_output
+mkdir -p results
